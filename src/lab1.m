3444ef956a762d1e77d29addb29ed798e12d0194
SabLiu <sabrinatliu@gmail.com>
2020-01-27 16:37:25 -0500

    Lab 1: Tuned PID values for joint 0 and 2. Plots 1 encoder on graph and csv

--- src/lab1.m
+++ src/lab1.m
@@ -1,18 +1,14 @@
 %%
-% RBE3001 - Laboratory 1 
-% 
+% RBE3001 - Laboratory 1
+%
 % Instructions
 % ------------
 % Welcome again! This MATLAB script is your starting point for Lab
 % 1 of RBE3001. The sample code below demonstrates how to establish
 % communication between this script and the Nucleo firmware, send
 % setpoint commands and receive sensor data.
-% 
-% IMPORTANT - understanding the code below requires being familiar
-% with the Nucleo firmware. Read that code first.
+%
 
-% Lines 15-37 perform necessary library initializations. You can skip reading
-% to line 38.
 clear
 clear java
 clear classes;
@@ -36,91 +32,87 @@ myHIDSimplePacketComs.setVid(vid);
 myHIDSimplePacketComs.connect();
 
 % Create a PacketProcessor object to send data to the nucleo firmware
-pp = PacketProcessor(myHIDSimplePacketComs); 
+pp = PacketProcessor(myHIDSimplePacketComs);
 try
-  PID_SERV_ID = 01;            % we will be talking to server ID 01 on
-                % the Nucleo
-
-  STATUS_SERV_ID = 02;
-  DEBUG   = true;          % enables/disables debug prints
-
-  % Instantiate a packet - the following instruction allocates 64
-  % bytes for this purpose. Recall that the HID interface supports
-  % packet sizes up to 64 bytes.
-  packet = zeros(15, 1, 'single');
-
-  % The following code generates a sinusoidal trajectory to be
-  % executed on joint 1 of the arm 
-%   viaPts = [0, -400, 400, -400, 400, 0];
-  viaPts = [140 6 -70; 330, 330, 150; -50, 200, 390];
-%     viaPts = [0];
-
-  for k = 0:2
-      tic
-      packet = zeros(15, 1, 'single');
-      packet((k*3)+1) = viaPts((k*3)+1);
-      pp.write(PID_SERV_ID, packet);
-      
-      % waits for robot arm and requests status for 1 second per command
-      for x = 0:99
-          timeStep = 0.01; 
-        % send a status request
+    %% Setup
+    % Define all server IDs
+    PID_SERV_ID = 01;
+    PID_CONFIG_SERV_ID = 02;
+    STATUS_SERV_ID = 10;
+    DEBUG   = true;          % enables/disables debug prints
+    
+    packet = zeros(15, 1, 'single');
+    
+    % Set PID values
+    % Joint 0 (works!)
+    packet(1) = 0.0015;
+    packet(2) = 0.0000095;
+    packet(3) = 0.000001;
+    % Joint 1
+    packet(4) = 0.2;
+    %   packet(5) = 0.00000001;
+    %   packet(6) = 0.00000001;
+    % Joint 2
+    packet(7) = 0.0044;
+    packet(8) = 0.0002;
+    packet(9) = 0.00009;
+    
+    pp.write(PID_CONFIG_SERV_ID,packet);
+    pause(0.003);
+    
+    %% Motor Control
+    % these are 3 arbitrary positions for all 3 joints
+    viaPts = [300];%[140 6 -70; 330, 330, 150; -50, 200, 390];
+    
+    for k = 0   %0:1
+        tic
         packet = zeros(15, 1, 'single');
-        pp.write(STATUS_SERV_ID, packet); 
-        pause(timeStep); % wait for response
-       returnPacket = pp.read(STATUS_SERV_ID);
-       % log values into CSV
-       printMatrix = zeros(1,4);
-       printMatrix(1) = x*timeStep+k; % time stamp for readings.  
-       for i = 0:3
-           % return packet contains encoder positions
-           % convert to angles and put into csv
-           printMatrix(i+2) = (returnPacket((i*3)+1)*2*pi)/4095;
-                           % this command for enc ticks returnPacket((i*3)+1);
-       end 
-       % Output to CSV
-        dlmwrite('test.csv', printMatrix, '-append');
-      end 
-
-%%%%%%%%%%%%%%this stuff is for plotting pretty pictures%%%%%%%%%%%%%%%%%%%
-
-%     hold on  
-%     angleLine = animatedline('Color','b', 'LineWidth', 2);
-% %     encoderLine = animatedline('Color','r', 'LineWidth', 2);
-%     % plots things twice
-%     timeInterval = 0.01;
-%     for x = 0:1000
-%         encPos = returnPacket(1);
-%          baseAngle = (encPos*2*pi)/4095; % returns radians 
-%           addpoints(angleLine, x*timeInterval, double(baseAngle));
-% %           addpoints(encoderLine, x, double(encPos));
-%             printMatrix = [x*timeInterval baseAngle encPos];
-%             dlmwrite('test.csv', printMatrix, '-append'); 
-%          pp.write(STATUS_SERV_ID, packet); 
-%         pause(timeInterval); 
-%         returnPacket = pp.read(STATUS_SERV_ID);
-%     end
-%       hold off
- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-      toc
-
-%       if DEBUG
-%           disp('Sent Packet:');
-%           disp(packet);
-%           disp('Received Packet:');
-%           disp(returnPacket);
-%       end
-%       
-%       for x = 0:3
-%           packet((x*3)+1)=0.1;
-%           packet((x*3)+2)=0;
-%           packet((x*3)+3)=0;
-%       end
-      
-      toc
-      
-  end
-  
+        packet(1) = -300;
+        packet(4) = 100;
+        packet(7) = 200;
+        
+        % will need to set positions with these for viaPts
+%         packet((k*3)+1) = viaPts((k*3)+1); % this is for joint 0
+%         packet((k*3)+4) = viaPts((k*3)+1); % this is for joint 0
+%         packet((k*3)+7) = viaPts((k*3)+1); % this is for joint 0
+        
+        % set motor to those positions
+        pp.write(PID_SERV_ID, packet);
+        
+        hold on
+        encoderLine = animatedline('Color','r', 'LineWidth', 2);
+        xlim([0 1]);
+        ylim([0,210]);
+        %% waits for robot arm and requests status
+        for x = 0:100
+            timeStep = 0.01;
+            
+            % send a status request
+            packet = zeros(15, 1, 'single');
+            pp.write(STATUS_SERV_ID, packet);
+            pause(timeStep); 
+            returnPacket = pp.read(STATUS_SERV_ID);
+            
+            % read encoder position from return packet
+            encPos = returnPacket(7); 
+            
+            % plot on graph
+            addpoints(encoderLine, x*timeStep, double(encPos));
+            
+            % log values into CSV
+            printMatrix = zeros(1,4);
+            printMatrix(1) = x*timeStep+k; % time stamp for readings.
+            for i = 0:3
+                printMatrix(i+2) = returnPacket((i*3)+1);
+                    % for angles: (returnPacket((i*3)+1)*2*pi)/4095;
+            end
+            dlmwrite('test.csv', printMatrix, '-append');
+        end
+        hold off
+        toc
+        
+    end
+    
 catch exception
     getReport(exception)
     disp('Exited on error, clean shutdown');
