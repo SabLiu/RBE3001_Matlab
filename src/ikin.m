7174d6e1aa3611ab0c6a3f09e6e869b7eb487b81
SabLiu <sabrinatliu@gmail.com>
2020-02-06 10:36:46 -0500

    Lab3: Fixing DH parameters. Now works as expected

--- src/ikin.m
+++ src/ikin.m
@@ -14,16 +14,13 @@ L1 = 135;
 L2 = 175; 
 L3 = 169.28;
 
-d=sqrt((px^2)+(py^2)+(pz-L1)^2);
+d = sqrt((px^2)+(py^2)+((pz-L1)^2));
 alpha = asin((pz-L1)/d);
 beta = acos(((L2^2)+(d^2)-(L3^2))/(2*L2*d));
 
 theta1 = atan2(py,px);
 theta2 = alpha + beta;
-theta3 = acos(-((L2^2)+(L3^2)-(d^2))/(2*L2*L3));
+theta3 = acos(((L2^2)+(L3^2)-(d^2))/(2*L2*L3));
 
-V = [-theta1; theta2; pi - theta3];
-% theta1 is in same direction as fwkin so keep it the same (negative)
-% theta 2 and 3 are opposite to fwkin so flip their signs (make them
-% positive)
+V = [-theta1; theta2; theta3-(pi/2)];
 end 
