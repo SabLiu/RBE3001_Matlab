133692c9a0435b6ed373c9eec33ff059e3e718d1
SabLiu <sabrinatliu@gmail.com>
2020-02-10 13:24:23 -0500

    Lab 3: fixed a small error with calculating quintic trajectory

--- src/quinticPositions.m
+++ src/quinticPositions.m
@@ -1,12 +1,13 @@
 % Take in 6x1 coefficients matrix e
-% Return encoder positions for 1 joint for 1 edge
+% Return 1x20 matrix, 20 encoder positions for 1 joint for 1 edge
 
-function a = quinticPositions(e)
-encoderPositions = zeros(); 
+% goal: take in coefficients, plug in times, get encoder values 
+function encoderPositions = quinticPositions(e)
+encoderPositions = zeros(1,20); 
 
 for i = 0:19
     t = i*0.1; 
-    q = e(1) + t*e(2) + (t^2)*e(3) + (t^3)*e(5) + (t^4)*e(6);
-    encoderPositions = [encoderPositions q]; % append to the right 
+    q = e(1) + t*e(2) + (t^2)*e(3) + (t^3)*e(4) + (t^4)*e(5) + (t^5)*e(6);
+    encoderPositions(1,i+1) = q; % append  
 end
 end
