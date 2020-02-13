14ebec666921c5475571f7d362e1fe13115e0fb0
SabLiu <sabrinatliu@gmail.com>
2020-02-13 16:00:36 -0500

    changed theta 1 back

--- src/fwkin3001.m
+++ src/fwkin3001.m
@@ -4,7 +4,7 @@ L1 = 135;
 L2 = 175;
 L3 = 169.28;
 % Find transformation matrices
-T1 = tdh(theta1, L1, 0, pi/2);
+T1 = tdh(-theta1, L1, 0, pi/2);
 T2 = tdh(theta2 , 0, L2, 0);
 T3 = tdh(theta3 -(pi/2), 0, L3, pi);
 % Calculate transformation matrix from 0 to 3
