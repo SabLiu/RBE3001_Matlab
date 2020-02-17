b9f8a4020316261588efb955ddf385bffcd50c82
SabLiu <sabrinatliu@gmail.com>
2020-02-17 16:06:33 -0500

    Lab 4: works! needed to flip sign for theta 1

--- src/invKinAlg.m
+++ src/invKinAlg.m
@@ -1,38 +1,27 @@
 
-% pe is desired position (3x1)
+% pd is desired position (3x1)
+% q0 is current thetas (3x1) 
+% qe is joint variables (thetas) needed to reach that position
 
-function jointAngles = invKinAlg()%pe)
-%% Setup
-    encoderToRadians = (2*pi)/4095; % encoder to theta
-    radiansToEncoder = 4095/(2*pi);
- % Plot stick model of robot at arbitrary configuration in xz plane
-    q0 = [0;pi/4; pi/6];
-    plot2DStickModel(q0);
-    
-    targetPoint = ginput(1); 
-    disp(targetPoint);
-    
-    pd = [targetPoint(1); 0; targetPoint(2)]; 
-    jointAngleTargets = ikin(pd); 
-    plot2DStickModel(jointAngleTargets); 
-    
-    fwkin = fwkin3001(0, jointAngleTargets(2), jointAngleTargets(3));
-    
-    disp(fwkin(1:3, 4));
-    
-    %% Move to target
-%     q = [returnPacket(1)*encoderToRadians; returnPacket(4)*encoderToRadians; returnPacket(7)*encoderToRadians]; 
-%     p0 = fwkin3001(returnPacket(1)*encoderToRadians, returnPacket(2)*encoderToRadians, returnPacket(7)*encoderToRadians);
-%     deltaQ = pe - p0;
-    
-    % while the robot is still 50 mm away from its desired position
-%     while (deltaQ > 50)
-%         % !!! need to define the invJacobian
-%         deltaQ = invJacobian*(pe - jacob0(q)); 
-%         
-%         q = [returnPacket(1)*encoderToRadians; returnPacket(4)*encoderToRadians; returnPacket(7)*encoderToRadians]; 
-%         p0 = fwkin3001(returnPacket(1)*encoderToRadians, returnPacket(2)*encoderToRadians, returnPacket(7)*encoderToRadians);
-%         deltaQ = pe - p0; 
-%         
-%     end
+function qi = invKinAlg(q0, pd)
+errorFK = fwkin3001(q0(1), q0(2), q0(3)); 
+error = pd - errorFK(1:3, 4);
+
+qi = q0; 
+count = 0;
+while (norm(error) > 1)
+    fw = fwkin3001(qi(1), qi(2), qi(3));
+    fq0 = fw(1:3, 4);
+    error = pd-fq0;
+    j = jacob0(qi);
+    deltaQ = pinv(j(1:3, 1:3))*error;
+    deltaQ(1) = -deltaQ(1);
+    qi = qi + deltaQ;
+    count = count + 1;
 end
+disp(qi); 
+n = fwkin3001(qi(1), qi(2), qi(3));
+disp(n); 
+
+end
+
