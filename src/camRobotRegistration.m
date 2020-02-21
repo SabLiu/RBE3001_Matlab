390f871b4f3a65c89a6975a2fc2afaa0f615cb38
SabLiu <sabrinatliu@gmail.com>
2020-02-21 16:55:27 -0500

    Lab5: works.

--- src/camRobotRegistration.m
+++ src/camRobotRegistration.m
@@ -1,6 +1,6 @@
 % ROBOT-CAMERA registration (step 3)
 %% Setup
-
+% run(CameraParams.m); 
 serverParams = initialize(); 
 PID_SERVER = serverParams(1); 
 STATUS_SERVER = serverParams(2); 
@@ -25,10 +25,6 @@ Pixel_values =   [308.3689  279.6416;
    65.7459  445.1352;
   127.3852  169.0837;
   483.4508  169.7704];
-% [307.6584  279.5625;
-%    65.4024  443.3125;
-%   127.1811  170.8125;
-%   483.2766  169.5625];
 
 % Tcam_0 = pinv(T0_cam);
 % worldPoints = pointsToWorld(cameraParams, Tcam_0(1:3,1:3), Tcam_0(1:3,end), Pixel_values);
@@ -36,9 +32,6 @@ Pixel_values =   [308.3689  279.6416;
 worldPoints = pointsToWorld(cameraParams, T_cam_to_checker(1:3,1:3), T_cam_to_checker(1:3,4), Pixel_values);
 disp('pixel to checkerboard ref');
 disp(worldPoints); 
-
-
-% disp(T_check_to_robot);
 
 TworldPoints = transpose(worldPoints); 
 roboPoints = T0_check * [TworldPoints; 15 15 15 15; 1 1 1 1];
@@ -49,15 +42,8 @@ disp(roboPoints);
 % image(origImg)
 % 
 % disp(ginput(4)); 
-% % 
 % grid on
 
-% second time pixel after 2nd calibration
-%   312.4844  278.6011
-%    70.1081  449.8989
-%   127.1967  172.3138
-%   486.7551  164.0585
-  
 % points wrt robot 0 - wrt checkerboard
 %(175,0) - (100.8, -113.6)
 %(279.5,-139) - (0, -249)
