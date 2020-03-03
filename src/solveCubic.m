2d9f3dff9b8f9682f569ef60cf1528bd5d11226e
SabLiu <sabrinatliu@gmail.com>
2020-03-03 14:42:30 -0500

    changed time steps.

--- src/solveCubic.m
+++ src/solveCubic.m
@@ -2,10 +2,10 @@
 % Takes in t, which is tf-to for that edge
 % returns 20 q values
 function encoderPositions = solveCubic(a)
-encoderPositions = zeros(1, 60); 
+encoderPositions = zeros(1, 50); 
 
-for i = 0:59
-    t = i*0.025; 
+for i = 0:49
+    t = i*0.05; 
     q = a(1) + a(2)*t + a(3)*(t^2) + a(4)*(t^3); 
     encoderPositions(1, i+1) = q; 
 end 
